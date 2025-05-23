a3c1aaa7636fbce4bca67a0a56752e3abe97c1b0
bhatakti-atama <malhotrajaisingh@gmail.com>
2025-05-23 12:07:04 +0530

    added truck command to babbu bhai help

--- triggers/babbu_bhai_help_trigger.py
+++ triggers/babbu_bhai_help_trigger.py
@@ -18,6 +18,7 @@ class BabbuBhaiHelpTrigger(Trigger):
         embed.add_field(name="`hoes mad`", value="🤬 Drops the legendary *hoes mad* meme whenever someone gets salty.", inline=False)
         embed.add_field(name="`jarvis [@user]`", value="🔧 Use this to 'deactivate' someone. Toto gone. Mission complete.", inline=False)
         embed.add_field(name="`na bhoreya kar`", value="🗣️ If someone writes a long essay (over 200 chars), Babbu Bhai will roast them.", inline=False)
+        embed.add_field(name="`truck [@user]`", value="🚚 Send isekai someone with truck-kun", inline=False)
 
         # No need to open with PIL if just sending raw content
         await message.channel.send(embed=embed)
